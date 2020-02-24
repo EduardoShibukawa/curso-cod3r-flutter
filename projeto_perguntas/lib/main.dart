bcaf4d6609c86bf2c96364086cd470b3bfcc417c
eduardoshibukawa <eduardoshibuka@gmail.com>
2020-02-24 00:40:05 -0300

    Bla

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
 main() => runApp(new PerguntaApp());
 
@@ -10,41 +11,42 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   void _responder() {
     setState(() {
-      _perguntaSelecionada++;  
+      _perguntaSelecionada++;
     });
-    
+
     print('Resposta respondida!!');
   }
 
   @override
-  Widget build(BuildContext context) {    
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'repostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leao']
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'Joao', 'Leo', 'Pedro']
+      },
     ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Perguntas"),          
+          title: Text("Perguntas"),
         ),
         body: Column(
           children: <Widget>[
             Questao(
-              texto: perguntas[_perguntaSelecionada],
+              texto: perguntas[_perguntaSelecionada]['texto'],
             ),
-             RaisedButton(
-               child: Text('Resposta 1'),
-               onPressed: _responder,
-             ),
-             RaisedButton(
-               child: Text('Resposta 2'),
-               onPressed: _responder,
-             ),
-             RaisedButton(
-               child: Text('Resposta 3'),
-               onPressed: _responder,
-             ),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
@@ -53,9 +55,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
 }
 
 class PerguntaApp extends StatefulWidget {
-  
+
   _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
-  
 }
