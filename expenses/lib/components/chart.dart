29813801acf3843a2a5863d9fa2ce2cb3608492c
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2020-05-03 18:05:18 -0300

    Criado chart

---
+++ expenses/lib/components/chart.dart
@@ -0,0 +1,58 @@
+import 'package:expenses/components/chart_bar.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (Transaction t in recentTransactions) {
+        if (t.date.difference(weekDay).inDays == 0) {
+          totalSum += t.value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0, (total, tr) {
+      return total + tr['value'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactions.map((tr) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  label: tr['day'],
+                  value: tr['value'],
+                  percentage: (tr['value'] as double) / _weekTotalValue,
+                ),
+              );
+            }).toList()),
+      ),
+    );
+  }
+}
